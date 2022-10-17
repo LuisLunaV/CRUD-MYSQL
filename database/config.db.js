54576af3c72e50b61bea5bdd908ec440bf9ded5e
LuisLunaV <luis.a.luna.v@outlook.com>
2022-10-17 01:39:09 -0500

    Se actualizaron las variables de entorno

--- database/config.db.js
+++ database/config.db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize( process.env.DATABASE, process.env.USER , process.env.PASS, {
-    host: 'localhost',
+const db = new Sequelize( process.env.NAME_DB, process.env.DB_USER , process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'mysql' 
 });
 
