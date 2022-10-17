a5508879758e6aba38d51b9ef55862726a7aaf4a
LuisLunaV <luis.a.luna.v@outlook.com>
2022-10-17 03:25:33 -0500

    Se actualiza database

--- database/config.db.js
+++ database/config.db.js
@@ -7,13 +7,7 @@ const db = new Sequelize( process.env.NAME_DB, process.env.DB_USER , process.env
 
 const dbConnection = async()=>{
     try {
-        console.log(process.env.NAME_DB)
-        console.log(process.env.DB_USER)
-        console.log(process.env.DB_PASS)
-        console.log(process.env.DB_HOST)
-        console.log(process.env.PORT)
 
-        console.log( db )
         await db.authenticate();
         console.log('Database en linea')
     } catch (error) {
