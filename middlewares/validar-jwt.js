b6f57ee94e4eee4b01a03f6d44347a4ab907a693
LuisLunaV <luis.a.luna.v@outlook.com>
2022-11-11 00:27:02 -0600

    Se agrega el validar-JWT.js

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -7,13 +7,13 @@ const validarJWT = async( req = request, res = response, next )=>{
 
     const token = req.header('x-token');
 
-     if( !token ){
+    //  if( !token ){
 
-        return res.status(401).json({
-            msg: 'No hay token de peticion'
-        })
+    //     return res.status(401).json({
+    //         msg: 'No hay token de peticion'
+    //     })
 
-    }
+    // }
 
     try {
 
