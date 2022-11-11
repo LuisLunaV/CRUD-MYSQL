3c0e1cb54e08db7e263acce8659cec5bab8e1dd3
LuisLunaV <luis.a.luna.v@outlook.com>
2022-11-10 23:27:20 -0600

    Se agrega el validar-JWT.js

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,56 @@
+const { request, response } = require('express');
+const jwt = require('jsonwebtoken');
+const { where } = require('../../09-socket-chat/models/usuario');
+
+const { User } = require('../models/user');
+
+const validarJWT = async( req = request, res = response, next )=>{
+
+    const token = req.header('x-token');
+
+    if( !token ){
+
+        return res.status(401).json({
+            msg: 'No hay token de peticion'
+        })
+
+    }
+
+    try {
+
+        const { uid } = jwt.verify( token, process.env.SECRETORPRIVATEKEY );
+        
+        //Leer el usuario que corresponde al uid
+        const user = await User.findOne({ where:{user_id: uid }} );
+        
+        //Verifica si el registro fue eliminado fisicamente de la BD
+       if( !user ){
+        return res.status(401).json({
+            msg:'Token no valido - usuario no existe en BD'
+        });
+       }
+
+       //Verificar si uid tiene estado en true //Eliminado
+       if( !user.user_estado ){
+        return res.status(401).json({
+            msg:'Token no valido - usuario con estado false '
+        });
+       }
+
+       req.user = user;
+       next();
+        
+    } catch (error) {
+        console.log(error)
+        res.status(401).json({
+            msg: 'Token no valido'
+        })
+    }
+
+    // next();
+
+}
+
+module.exports = {
+    validarJWT
+}
