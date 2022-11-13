ede9cfdee25d0f0cf0ce959cd626559cd2cc502c
LuisLunaV <luis.a.luna.v@outlook.com>
2022-11-13 00:00:16 -0600

    cors

--- controllers/auth.controler.js
+++ controllers/auth.controler.js
@@ -33,6 +33,7 @@ const login = async( req, res=response ) => {
         const token = await generarJwt( user.user_id );
 
         return res.json({
+            token,
             user
         });
 
