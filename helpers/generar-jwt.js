78d54c837a8935fa07236fb0a50ceb47c317e212
LuisLunaV <luis.a.luna.v@outlook.com>
2022-11-12 23:56:51 -0600

    cors

--- helpers/generar-jwt.js
+++ helpers/generar-jwt.js
@@ -5,11 +5,9 @@ const generarJwt =( uid )=>{
 return new Promise((resolve, reject)=>{
 
     const payload = { uid }
-console.log(payload)
+console.log(process.env.SECRETORPRIVATEKEY)
     jwt.sign(payload, process.env.SECRETORPRIVATEKEY, {
-    
         expiresIn: '4h'
-    
     }, (err, token)=>{
     
         if( err ){
