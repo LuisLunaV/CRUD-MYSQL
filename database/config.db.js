14e5d3973184c3e381660f53e23f75c3f726ca74
LuisLunaV <luis.a.luna.v@outlook.com>
2023-03-14 22:29:43 -0600

    Agregamos la nueva variable de entorno a la instancia de sequelize

--- database/config.db.js
+++ database/config.db.js
@@ -1,14 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize( process.env.NAME_DB, process.env.DB_USER , process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: true,        
-        }
-    }
-});
+const db = new Sequelize( process.env.DB_URL);
 
 const dbConnection = async()=>{
     try {
